9b9d38d2029ca10fb62db276975f4032837a7282
Gucioo <tomasz.baryla@gmail.com>
2025-08-30 22:21:38 +0100

    Update __init__.py
    esphome tcp async

--- components/modbus_tcp_16/sensor/__init__.py
+++ components/modbus_tcp_16/sensor/__init__.py
@@ -1,45 +1,59 @@
 import esphome.codegen as cg
-from esphome.components import sensor
 import esphome.config_validation as cv
+from esphome.components import sensor
+from esphome.const import (
+    CONF_HOST,
+    CONF_ID,
+    CONF_PORT,
+    CONF_UPDATE_INTERVAL,
+    DEVICE_CLASS_EMPTY,
+    STATE_CLASS_MEASUREMENT,
+    UNIT_EMPTY,
+)
 
+# Dependencies - CRITICAL for networking
 DEPENDENCIES = ["wifi", "network"]
+CODEOWNERS = ["@Gucioo"]
 
-CODEOWNERS = ["@creepystefan"]
-
-CONF_FUNCTIONCODE =  'functioncode'
-CONF_REGISTER_ADDRESS =  'register_address'
-#CONF_UPDATE_INTERVAL =  'update_interval'
-CONF_IP_ADDRESS = 'host'
-CONF_PORT = 'port'
-
-
+# Namespace and class definitions
 modbus_tcp_16_ns = cg.esphome_ns.namespace("modbus_tcp_16")
-ModbusTCP16 = modbus_tcp_16_ns.class_("ModbusTCP16", sensor.Sensor, cg.PollingComponent)
+ModbusTCP16 = modbus_tcp_16_ns.class_("ModbusTCP16", cg.Component, sensor.Sensor)
 
+# Configuration schema
+CONF_SLAVE_ID = "slave_id"
+CONF_REGISTER_ADDRESS = "register_address"
+CONF_REGISTER_COUNT = "register_count"
 
-CONFIG_SCHEMA = cv.Schema(
-    sensor.sensor_schema(ModbusTCP16)
+CONFIG_SCHEMA = (
+    sensor.sensor_schema(
+        ModbusTCP16,
+        device_class=DEVICE_CLASS_EMPTY,
+        state_class=STATE_CLASS_MEASUREMENT,
+        unit_of_measurement=UNIT_EMPTY,
+    )
     .extend(
         {
-        cv.GenerateID(): cv.declare_id(ModbusTCP16),
-        cv.Required(CONF_IP_ADDRESS): cv.ipv4address,
-        cv.Optional(CONF_PORT, default=502): cv.int_range(0, 65535),
-        #cv.Optional(CONF_FUNCTIONCODE, default=4): cv.int_range(min=0, max=10),
-        cv.Required(CONF_FUNCTIONCODE): cv.int_range(min=0, max=4),
-        cv.Required(CONF_REGISTER_ADDRESS): cv.int_,
-     #   cv.Optional(CONF_UPDATE_INTERVAL, default=1000): cv.int_range(min=500, max=10000),
+            cv.Required(CONF_HOST): cv.string,
+            cv.Optional(CONF_PORT, default=502): cv.port,
+            cv.Optional(CONF_SLAVE_ID, default=1): cv.uint8_t,
+            cv.Required(CONF_REGISTER_ADDRESS): cv.uint16_t,
+            cv.Optional(CONF_REGISTER_COUNT, default=1): cv.uint16_t,
+            cv.Optional(CONF_UPDATE_INTERVAL, default="60s"): cv.update_interval,
         }
-       ).extend(cv.COMPONENT_SCHEMA)
-       .extend(cv.polling_component_schema("1s")))
-       
-
+    )
+    .extend(cv.COMPONENT_SCHEMA)
+)
 
 async def to_code(config):
-    #var = cg.new_Pvariable(config[CONF_ID])
     var = await sensor.new_sensor(config)
     await cg.register_component(var, config)
-    cg.add(var.set_host(str(config[CONF_IP_ADDRESS])))
-    cg.add(var.set_functioncode(config[CONF_FUNCTIONCODE]))
+
+    cg.add(var.set_host(config[CONF_HOST]))
+    cg.add(var.set_port(config[CONF_PORT]))
+    cg.add(var.set_slave_id(config[CONF_SLAVE_ID]))
     cg.add(var.set_register_address(config[CONF_REGISTER_ADDRESS]))
-    cg.add(var.set_port(config["port"]))
-   # cg.add(var.update_interval(config[CONF_UPDATE_INTERVAL]))
+    cg.add(var.set_register_count(config[CONF_REGISTER_COUNT]))
+    cg.add(var.set_update_interval(config[CONF_UPDATE_INTERVAL].total_milliseconds))
+
+    # Add library dependencies for AsyncTCP
+    cg.add_library("AsyncTCP", "1.1.1")
